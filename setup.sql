2ff3b4d5a872dead6cbdf5dd2e5fe2494097b9d4
Darryl Kilzer <darrylkilzer@gmail.com>
2020-09-02 11:58:47 -0600

    handoff

---
+++ setup.sql
@@ -0,0 +1,18 @@
+/* CREATE TABLE cars (
+    id INT NOT NULL AUTO_INCREMENT,
+    make VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    year INT,
+    imgUrl VARCHAR(255) NOT NULL,
+    price INT,
+    description VARCHAR(255),
+    userId VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+) */
+
+
+/* 
+INSERT INTO cars
+(make, model, year, price, imgUrl, description, userId)
+VALUES
+("Chevy", "Tracker", 1989, 2000, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQiEdhq7a4aHGvv8FxjKUpAg_godVfcyLzHSg&usqp=CAU", "Itsa trackin", "D$SUPERSECRETUSERID") */
